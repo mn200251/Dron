49ce457968cb46f2c1b2baa048540c75a7d97a1b
Marko Rabat <rabatmarko@gmail.com>
2024-07-23 01:03:53 +0200

    added drone model to the simulator, physics partial

--- drone_simulation/main.py
+++ drone_simulation/main.py
@@ -1,131 +1,62 @@
+from os import environ
+environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 import pygame
 import numpy as np
 import drone_model as dm
+import projection_math as pm
+import physics_engine as pe
 
 pygame.init()
+pygame.display.set_caption("drone simulator")
+pygame.display.set_icon(pygame.image.load("./drone_simulation/drone_icon.png"))
+
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 500
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 bg_color = (255, 255, 255)
 
-camera_position = np.array([0, SCREEN_HEIGHT, 0, 1])
-space_coordinate_system_base = np.array([SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 10, 1])
+# SCREEN PARAMETERS
+canonical_near_plane_center = np.array([SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 0], dtype=float)
+near_plane_center = np.array([0, 0, 50, 1], dtype=float)
+canonical_volume_size = np.array([SCREEN_WIDTH, SCREEN_HEIGHT, 100], dtype=float)
+orthographic_volume_size = np.array([50, 50, 50], dtype=float)
+viewer_scene_distance = 10
 
-def create_projection_matrix(fov_angel, aspect, znear, zfar):
-    return np.array([
-        [aspect / np.tan(fov_angel / 2), 0, 0, 0],
-        [0, 1 / np.tan(fov_angel / 2), 0, 0],
-        [0, 0, zfar / (zfar - znear), - zfar * znear / (zfar - znear)],
-        [0, 0, 1, 0]
-    ])
-projection_matrix =  create_projection_matrix(np.pi/4, 1, 0, 30)
+orthographic_volume_center = \
+    near_plane_center + np.array([0, 0, orthographic_volume_size[2] / 2, 0], dtype=float)
 
-def project_to_camera_screen(vector_in_camera_space):
-    vector_in_camera_space = projection_matrix.dot(vector_in_camera_space)
-    if vector_in_camera_space[3] > 0.00000001:
-        vector_in_camera_space /= vector_in_camera_space[3]
-    vector_in_camera_space += space_coordinate_system_base
-    return np.array([camera_position[0] + vector_in_camera_space[0], camera_position[1] - vector_in_camera_space[1]])
+pr = pm.Projector(
+    canonical_near_plane_center=canonical_near_plane_center,
+    near_plane_center=near_plane_center,
+    canonical_volume_size=canonical_volume_size,
+    orthographic_volume_size=orthographic_volume_size,
+    viewer_scene_distance=viewer_scene_distance
+)
 
-origin = project_to_camera_screen(np.array([0, 0, 0, 1]))
-base_x = project_to_camera_screen(np.array([50, 0, 0, 1]))
-base_y = project_to_camera_screen(np.array([0, 50, 0, 1]))
-base_z = project_to_camera_screen(np.array([0, 0, 50, 1]))
+drone = dm.Drone(orthographic_volume_center, 50, pr)
 
-a = 50
-A_raw = np.array([-a, 0, 0, 1.])
-B_raw = np.array([a, 0, 0, 1.])
-C_raw = np.array([0, a * np.sqrt(3), 0, 1.])
-D_raw = np.array([0, a * np.sqrt(3) / 3, a * 2 * np.sqrt(6) / 3 , 1.])
+def draw_simulator_state():
+    screen.fill(bg_color)
+    drone.draw_to_(screen)
+    pygame.display.flip()
 
-zoffset = np.array([0, 0, a * np.sqrt(3) / 3, 0])
-A_raw += zoffset
-B_raw += zoffset
-C_raw += zoffset
-D_raw += zoffset
-
-xrot_angle = 0
-xrot_angle = xrot_angle * np.pi / 180
-Rx = np.array([
-    [1, 0, 0, 0],
-    [0, np.cos(xrot_angle), -np.sin(xrot_angle), 0],
-    [0, np.sin(xrot_angle), np.cos(xrot_angle), 0],
-    [0, 0, 0, 1],
-])
-
-yrot_angle = 10
-yrot_angle = yrot_angle * np.pi / 180
-Ry = np.array([
-    [np.cos(yrot_angle), 0, np.sin(yrot_angle), 0],
-    [0, 1, 0, 0],
-    [-np.sin(yrot_angle), 0, np.cos(yrot_angle), 0],
-    [0, 0, 0, 1]
-])
-
-zrot_angle = 0
-zrot_angle = zrot_angle * np.pi / 180
-Rz = np.array([
-    [np.cos(zrot_angle), -np.sin(zrot_angle), 0, 0],
-    [np.sin(zrot_angle), np.cos(zrot_angle), 0, 0],
-    [0, 0, 1, 0],
-    [0, 0, 0, 1]
-])
-
-rotation_matrix = Rx.dot(Ry.dot(Rz))
-
+drone.rotate(-15, [0, 0, 1])
+drone.rotate(-15, [1, 0, 0])
+drone.rotate(-145, [0, 1, 0])
 
 running = True
-val = 250
-cnt = val
-
-A = project_to_camera_screen(A_raw)
-B = project_to_camera_screen(B_raw)
-C = project_to_camera_screen(C_raw)
-D = project_to_camera_screen(D_raw)
 while running:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT: running = False
     if not running: break
 
-    if (cnt == 0):
-        A_raw = rotation_matrix.dot(A_raw)
-        B_raw = rotation_matrix.dot(B_raw)
-        C_raw = rotation_matrix.dot(C_raw)
-        D_raw = rotation_matrix.dot(D_raw)
-        A = project_to_camera_screen(A_raw)
-        B = project_to_camera_screen(B_raw)
-        C = project_to_camera_screen(C_raw)
-        D = project_to_camera_screen(D_raw)
-        print("RAW  A,B,C,D:")
-        print(A_raw)
-        print(B_raw)
-        print(C_raw)
-        print(D_raw)
-        cnt = val
-    cnt -= 1
-    if cnt == val - 1:
-        print("A,B,C,D:")
-        print(A)
-        print(B)
-        print(C)
-        print(D)
+    drone.rotate(-0.03, [0, 1, 0])
+    #drone.rotate(-0.15, [1, 0, 0])
+    drone.motor_set_force_percent(0, -1)
+    drone.motor_set_force_percent(1, 1)
+    drone.motor_set_force_percent(2, -1)
+    drone.motor_set_force_percent(3, 1)
+    drone.update()
 
-
-    screen.fill(bg_color)
-    pygame.draw.line(screen, (0, 0, 0), tuple(origin), tuple(base_x))
-    pygame.draw.line(screen, (0, 0, 0), tuple(origin), tuple(base_y))
-    pygame.draw.line(screen, (0, 0, 0), tuple(origin), tuple(base_z))
-    pygame.draw.line(screen, (255, 0, 0), tuple(A[:2]), tuple(B[:2]))
-    pygame.draw.line(screen, (255, 0, 0), tuple(A[:2]), tuple(B[:2]))
-    pygame.draw.line(screen, (255, 0, 0), tuple(A[:2]), tuple(C[:2]))
-    pygame.draw.line(screen, (255, 0, 0), tuple(A[:2]), tuple(D[:2]))
-    pygame.draw.line(screen, (255, 0, 0), tuple(B[:2]), tuple(C[:2]))
-    pygame.draw.line(screen, (255, 0, 0), tuple(B[:2]), tuple(D[:2]))
-    pygame.draw.line(screen, (255, 0, 0), tuple(C[:2]), tuple(D[:2]))
-    pygame.draw.line(screen, (255, 0, 0), tuple((A[:2] + B[:2]) / 2), tuple(D[:2]))
-    pygame.draw.line(screen, (255, 0, 0), tuple((A[:2] + B[:2]) / 2), tuple(C[:2]))
-    pygame.display.flip()
-
-
-
+    draw_simulator_state()
---
+++ drone_simulation/physics_engine.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def rotation_matrix_factory(angle, unit_vector, degrees=False):
+    if degrees: angle = angle * np.pi / 180
+    unit_vector =     axis_rotation_matrix = np.array([
+        [np.cos(angle) + unit_vector[0] * unit_vector[0] * (1 - np.cos(angle)), unit_vector[0] * unit_vector[1] * (1 - np.cos(angle)) - unit_vector[2] * np.sin(angle), unit_vector[0] * unit_vector[2] * (1 - np.cos(angle)) + unit_vector[1] * np.sin(angle), 0],
+        [unit_vector[1] * unit_vector[0] * (1 - np.cos(angle)) + unit_vector[2] * np.sin(angle), np.cos(angle) + unit_vector[1] * unit_vector[1] * (1 - np.cos(angle)), unit_vector[1] * unit_vector[2] * (1 - np.cos(angle)) - unit_vector[0] * np.sin(angle), 0],
+        [unit_vector[2] * unit_vector[0] * (1 - np.cos(angle)) - unit_vector[1] * np.sin(angle), unit_vector[2] * unit_vector[1] * (1 - np.cos(angle)) + unit_vector[0] * np.sin(angle), np.cos(angle) + unit_vector[2] * unit_vector[2] * (1 - np.cos(angle)), 0],
+        [0, 0, 0, 1]
+    ])
+    return axis_rotation_matrix
+
+def cross_product(u, v):
+    return np.array([u[1] * v[2] - u[2] * v[1], u[2] * v[0] - u[0] * v[2], u[0] * v[1] - u[1] * v[0], 1], dtype=float)
+
+def normalize_vector(v):
+    if v.sum() == 0: return v
+    return v / np.sqrt(np.array([v[0] * v[0], v[1] * v[1], v[2] * v[2], v[3] * v[3]], dtype=float).sum())
+
+if __name__ == "__main__":
+
+    rotation_matrix = rotation_matrix_factory(0.05, np.array([0, 2, 2.7]) / np.sqrt(4 + 2.7**2))
+    print(rotation_matrix)
