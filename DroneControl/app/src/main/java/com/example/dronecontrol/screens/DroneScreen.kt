aa09c973ff2b5dbee68c44d0d6085a238e32106d
Nikola Mirkovic <mn200251d@student.etf.bg.ac.rs>
2024-09-23 23:11:16 +0200

    Moved power button little to the right

--- DroneControl/app/src/main/java/com/example/dronecontrol/screens/DroneScreen.kt
+++ DroneControl/app/src/main/java/com/example/dronecontrol/screens/DroneScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
@@ -145,6 +146,8 @@ fun DroneScreen(
                     tint = Color(64, 64, 64, 75),
                 )
             }
+            
+            Spacer(modifier = Modifier.padding(10.dp))
 
             Box(
                 modifier = Modifier
